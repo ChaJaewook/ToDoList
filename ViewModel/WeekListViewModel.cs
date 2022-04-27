4f5ed0c325649ae1503d4b84dbe38709e0b884d8
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2022-04-27 23:57:09 +0900

    - Week폼 제작 중

---
+++ ViewModel/WeekListViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using ToDoList.Util;
+namespace ToDoList.ViewModel
+{
+    public class WeekListViewModel:NotifyChanged
+    {
+        #region 변수
+        AcsDBManager _manager;
+        string _query = string.Empty;
+        #endregion
+        #region Binding 변수정리
+        private string weekDate;
+        public string WeekDate
+        {
+            get { return weekDate; }
+            set
+            {
+                weekDate = value;
+                OnPropertyChanged("WeekDate");
+            }
+        }
+
+        private ObservableCollection<TextBlock> weekCheckList = new ObservableCollection<TextBlock>();
+        public ObservableCollection<TextBlock> WeekCheckList
+        {
+            get
+            {
+                return weekCheckList;
+            }
+            set
+            {
+                weekCheckList = value;
+                OnPropertyChanged("WeekCheckList");
+            }
+        }
+
+
+
+        #endregion
+        public WeekListViewModel()
+        {
+            _manager = new AcsDBManager();
+            
+           
+
+        }
+
+        public void Load()
+        {
+            _manager.OpenDB();
+            string startDate = DateTime.Today.AddDays(Convert.ToInt32(DayOfWeek.Monday) - Convert.ToInt32(DateTime.Today.DayOfWeek)).ToString("yyyy-MM-dd");
+            string endDate= DateTime.Today.AddDays(Convert.ToInt32(DayOfWeek.Sunday) - Convert.ToInt32(DateTime.Today.DayOfWeek)).ToString("yyyy-MM-dd");
+
+            _query = string.Format("select * from ListTable BETWEEN '{0}' and '{1} orderby DoDate_'", startDate, endDate);
+
+            /*for(int i=0;i<7;i++)
+            {
+                string searchDate = startDate.AddDays(i).ToString("yyyy-MM-dd");
+                _query=string.Format("select * from where")
+
+
+            }*/
+            _manager.CloseDB();
+        }
+    }
+}
