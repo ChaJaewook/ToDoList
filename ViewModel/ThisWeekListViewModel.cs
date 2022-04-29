695dd630dcb7d3e663c080146823d54ce712b67a
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2022-04-29 23:53:03 +0900

    - 이번 주 할일 페이지 제작 진행 중

---
+++ ViewModel/ThisWeekListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ToDoList.ViewModel
+{
+    public class ThisWeekListViewModel:NotifyChanged
+    {
+        #region Binding 정의
+        ObservableCollection<ItemsControl> thisWeekList = new ObservableCollection<ItemsControl>();
+        ObservableCollection<ItemsControl> ThisWeekList
+        {
+            get { return thisWeekList; }
+            set
+            {
+                thisWeekList = value;
+                OnPropertyChanged("ThisWeekList");
+            }
+        }
+        #endregion
+        public ThisWeekListViewModel()
+        {
+            
+        }
+    }
+}
--- ViewModel/WeekListViewModel.cs
+++ ViewModel/WeekListViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace ToDoList.ViewModel
     {
         #region 변수
         AcsDBManager _manager;
+        DataSet _result;
         string _query = string.Empty;
         #endregion
         #region Binding 변수정리
@@ -47,18 +49,17 @@ namespace ToDoList.ViewModel
         {
             _manager = new AcsDBManager();
             
-           
-
         }
 
         public void Load()
         {
+            _result = new DataSet();
             _manager.OpenDB();
             string startDate = DateTime.Today.AddDays(Convert.ToInt32(DayOfWeek.Monday) - Convert.ToInt32(DateTime.Today.DayOfWeek)).ToString("yyyy-MM-dd");
             string endDate= DateTime.Today.AddDays(Convert.ToInt32(DayOfWeek.Sunday) - Convert.ToInt32(DateTime.Today.DayOfWeek)).ToString("yyyy-MM-dd");
 
             _query = string.Format("select * from ListTable BETWEEN '{0}' and '{1} orderby DoDate_'", startDate, endDate);
-
+            _result = _manager.Select(_query);
             /*for(int i=0;i<7;i++)
             {
                 string searchDate = startDate.AddDays(i).ToString("yyyy-MM-dd");
