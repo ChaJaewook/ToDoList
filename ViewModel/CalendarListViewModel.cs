dfe1c1d78680285b46e887616bca8e8bcd5ee785
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2022-06-01 23:52:42 +0900

    - Calendar관련 버튼 제작 진행

--- ViewModel/CalendarListViewModel.cs
+++ ViewModel/CalendarListViewModel.cs
@@ -5,12 +5,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
+using ToDoList.ViewModel.Command;
 
 namespace ToDoList.ViewModel
 {
     public class CalendarListViewModel:NotifyChanged
     {
-        /*public ObservableCollection<ItemsControl> calendarList = new ObservableCollection<ItemsControl>();
+        #region Command 변수정의
+        private ICommand prevMonthButton;
+        public ICommand PrevMonthButton
+        {
+            get
+            {
+                return (this.prevMonthButton) ?? (this.prevMonthButton = new DelegateCommand(PrevMonthButtonExecute));
+            }
+        }
+
+
+
+        private ICommand nextMonthButton;
+        public ICommand NextMonthButton
+        {
+            get
+            {
+                return (this.nextMonthButton) ?? (this.nextMonthButton = new DelegateCommand(NextMonthButtonExecute));
+            }
+        }
+
+        private void NextMonthButtonExecute()
+        {
+            
+        }
+
+        private void PrevMonthButtonExecute()
+        {
+            
+        }
+
+        #endregion
+
+        #region Binding 변수정의
+        public ObservableCollection<ItemsControl> calendarList = new ObservableCollection<ItemsControl>();
         public ObservableCollection<ItemsControl> CalendarList
         {
             get { return calendarList; }
@@ -19,7 +55,39 @@ namespace ToDoList.ViewModel
                 calendarList = value;
                 OnPropertyChanged("ThisWeekList");
             }
-        }*/
+        }
+
+        
+
+        private string calendarYear;
+        public string CalendarYear
+        {
+            get
+            {
+                return calendarYear;
+            }
+            set
+            {
+                calendarYear = value;
+                OnPropertyChanged("CalendarYear");
+            }
+        }
+
+        private string calendarMonth;
+        public string CalendarMonth
+        {
+            get
+            {
+                return calendarMonth;
+            }
+            set
+            {
+                calendarMonth = value;
+                OnPropertyChanged("CalendarMonth");
+            }
+        }
+        #endregion
+
         public CalendarListViewModel()
         {
 
@@ -28,7 +96,10 @@ namespace ToDoList.ViewModel
 
         public void Load()
         {
-
+            CalendarYear = DateTime.Now.ToString("yyyy");
+            CalendarMonth = DateTime.Now.ToString("MM");
         }
+
+
     }
 }
